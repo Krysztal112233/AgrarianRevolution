3b091fe4e902a61def99f784a66f2c7799559d63
Krysztal <suibing112233@outlook.com>
2022-07-20 05:27:20 +0800

    ✨ Feat: Add bamboo bundles' dryness process.

--- src/main/java/dev/krysztal/advagri/AdvAgriConstants.java
+++ src/main/java/dev/krysztal/advagri/AdvAgriConstants.java
@@ -4,4 +4,5 @@ public class AdvAgriConstants {
 
     public static final String ADV_AGRI_NAMESPACE = "adv_agri";
     public static final String ADV_AGRI_NAMESPACE_ENTITY = "adv_agri_entity";
+    public static final int MAX_DRYNESS = 4;
 }
---
+++ src/main/java/dev/krysztal/advagri/AdvAgriStates.java
@@ -0,0 +1,4 @@
+package dev.krysztal.advagri;
+
+public class AdvAgriStates {
+}
