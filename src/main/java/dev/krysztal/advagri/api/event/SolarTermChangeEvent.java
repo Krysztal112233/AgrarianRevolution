6ea1270812446b07dc4e37325354aa1676c4fc62
Krysztal <suibing112233@outlook.com>
2022-11-06 00:49:46 +0800

    ✨ Feat: Add event. - Add `SolarTermChangeEvent.AFTER` event. - Add `SolarTermChangeEvent.BEFORE` event

---
+++ src/main/java/dev/krysztal/advagri/api/event/SolarTermChangeEvent.java
@@ -0,0 +1,38 @@
+package dev.krysztal.advagri.api.event;
+
+import dev.krysztal.advagri.foundation.term.AdvAgriSeason;
+import java.util.Arrays;
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
+
+public class SolarTermChangeEvent {
+
+  public static Event<SeasonChange> BEFORE = EventFactory.createArrayBacked(
+    SeasonChange.class,
+    listeners ->
+      (minecraftServer, serverWorld, season) ->
+        Arrays
+          .stream(listeners)
+          .forEach($ -> $.interact(minecraftServer, serverWorld, season))
+  );
+
+  public static Event<SeasonChange> AFTER = EventFactory.createArrayBacked(
+    SeasonChange.class,
+    listeners ->
+      (minecraftServer, serverWorld, season) ->
+        Arrays
+          .stream(listeners)
+          .forEach($ -> $.interact(minecraftServer, serverWorld, season))
+  );
+
+  @FunctionalInterface
+  public interface SeasonChange {
+    public void interact(
+      MinecraftServer minecraftServer,
+      ServerWorld serverWorld,
+      AdvAgriSeason season
+    );
+  }
+}
