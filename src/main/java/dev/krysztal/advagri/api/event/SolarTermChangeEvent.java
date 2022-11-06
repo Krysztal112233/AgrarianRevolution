46db1d6c8b44ba364c3770d7ec0fc14947c018ba
Krysztal <suibing112233@outlook.com>
2022-11-06 23:56:22 +0800

    🔨 Fix: Add @SuppressWarns("unused") - SolarTermChangeEvent

--- src/main/java/dev/krysztal/advagri/api/event/SolarTermChangeEvent.java
+++ src/main/java/dev/krysztal/advagri/api/event/SolarTermChangeEvent.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.event.EventFactory;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
 
+@SuppressWarnings("unused")
 public class SolarTermChangeEvent {
 
   public static Event<SeasonChange> BEFORE = EventFactory.createArrayBacked(
