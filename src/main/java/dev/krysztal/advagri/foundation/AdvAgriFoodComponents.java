0f6981a6114d744ecc7558bc025178d1f6139536
Krysztal <suibing112233@outlook.com>
2022-12-04 22:01:30 +0800

    ✨ Feat: Add FoodComponent for water chestnut.
    - Add FoodComponent `WATER_CHESTNUT` for water chestnut

--- src/main/java/dev/krysztal/advagri/foundation/AdvAgriFoodComponents.java
+++ src/main/java/dev/krysztal/advagri/foundation/AdvAgriFoodComponents.java
@@ -10,40 +10,52 @@ public class AdvAgriFoodComponents {
   public static final FoodComponent PEELED_BAMBOO_SAPLING;
   public static final FoodComponent SWEET_POTATO;
   public static final FoodComponent SOLAR_BERRY;
+  public static final FoodComponent WATER_CHESTNUT;
 
   static {
     ASPARAGUS =
       new FoodComponent.Builder()
-        .meat()
-        .snack()
         .hunger(4)
         .saturationModifier(0.6F)
+        .meat()
+        .snack()
         .build();
 
     PEELED_BAMBOO_SAPLING =
       new FoodComponent.Builder()
         .hunger(2)
-        .snack()
         .saturationModifier(0.2F)
+        .snack()
         .build();
 
     SWEET_POTATO =
       new FoodComponent.Builder()
         .hunger(4)
-        .snack()
         .saturationModifier(0.6F)
+        .snack()
         .build();
 
     SOLAR_BERRY =
       new FoodComponent.Builder()
         .hunger(2)
         .saturationModifier(0.1F)
-        .alwaysEdible()
         .statusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 60), 1)
         .statusEffect(
           new StatusEffectInstance(StatusEffects.NIGHT_VISION, 120),
           1
         )
+        .alwaysEdible()
+        .build();
+
+    WATER_CHESTNUT =
+      new FoodComponent.Builder()
+        .hunger(4)
+        .saturationModifier(0.7F)
+        .statusEffect(
+          new StatusEffectInstance(StatusEffects.POISON, 160),
+          0.03F
+        )
+        .alwaysEdible()
         .build();
   }
 }
