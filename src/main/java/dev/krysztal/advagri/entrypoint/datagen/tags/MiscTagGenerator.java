a4b09b6da6e03bedb033c74d80f2d485fa233bd4
Krysztal <suibing112233@outlook.com>
2022-12-07 20:23:45 +0800

    🔨 Fix: Fix build error.

--- src/main/java/dev/krysztal/advagri/entrypoint/datagen/tags/MiscTagGenerator.java
+++ src/main/java/dev/krysztal/advagri/entrypoint/datagen/tags/MiscTagGenerator.java
@@ -5,10 +5,23 @@ import java.util.List;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.tag.ItemTags;
+import net.minecraft.tag.TagKey;
 
 public class MiscTagGenerator extends FabricTagProvider.ItemTagProvider {
 
-  private static final List<Item> PLANT_TAG_LIST = List.of();
+  private static final List<Item> PLANT_LIST = List.of(
+    Items.FERN,
+    Items.DEAD_BUSH,
+    Items.KELP,
+    Items.SEA_PICKLE,
+    Items.SEAGRASS
+  );
+  private static final List<TagKey<Item>> PLANT_TAG_LIST = List.of(
+    ItemTags.LEAVES,
+    ItemTags.FLOWERS
+  );
 
   public MiscTagGenerator(FabricDataGenerator dataGenerator) {
     super(dataGenerator);
@@ -17,6 +30,10 @@ public class MiscTagGenerator extends FabricTagProvider.ItemTagProvider {
   @Override
   protected void generateTags() {
     PLANT_TAG_LIST.forEach(
+      super.getOrCreateTagBuilder(AdvAgriTags.ItemTags.PLANT)::addOptionalTag
+    );
+
+    PLANT_LIST.forEach(
       super.getOrCreateTagBuilder(AdvAgriTags.ItemTags.PLANT)::add
     );
   }
