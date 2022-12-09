e4968b3ce05baa6a702ba5e1f771aaa0ff6145e2
Krysztal <suibing112233@outlook.com>
2022-12-09 20:46:58 +0800

    ✨ Feat: Add `AdvAgriRegistrates`
    - Add `AdvAgriRegistrates.ITEM_REGISTRATE` for item registry.
    - Add `AdvAgriRegistrates.BLOCK_REGISTRATE` for block registry.
    - Add `AdvAgriRegistrates.FOOD_REGISTRATE` for food item registry.

---
+++ src/main/java/dev/krysztal/advagri/foundation/AdvAgriRegistrates.java
@@ -0,0 +1,18 @@
+package dev.krysztal.advagri.foundation;
+
+import com.tterrag.registrate.Registrate;
+
+public class AdvAgriRegistrates {
+
+  public static final Registrate ITEM_REGISTRATE = Registrate.create(
+    AdvAgriConstants.ADV_AGRI_MODID
+  );
+
+  public static final Registrate BLOCK_REGISTRATE = Registrate.create(
+    AdvAgriConstants.ADV_AGRI_MODID
+  );
+  
+  public static final Registrate FOOD_REGISTRATE = Registrate.create(
+    AdvAgriConstants.ADV_AGRI_MODID
+  );
+}
