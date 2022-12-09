f7b2bc2bbb4521bf5e7378922d6f2f1f55c308a2
Krysztal <suibing112233@outlook.com>
2022-12-09 17:24:20 +0800

    🔨 Refact: Rename `ADV_AGRI_NAMESPACE_ENTITY`
    Rename to `ADV_AGRI_ENTITY_KEY`

--- src/main/java/dev/krysztal/advagri/foundation/AdvAgriConstants.java
+++ src/main/java/dev/krysztal/advagri/foundation/AdvAgriConstants.java
@@ -3,5 +3,5 @@ package dev.krysztal.advagri.foundation;
 public class AdvAgriConstants {
 
   public static final String ADV_AGRI_MODID = "advagri";
-  public static final String ADV_AGRI_NAMESPACE_ENTITY = "advagri_entity";
+  public static final String ADV_AGRI_ENTITY_KEY = "advagri_entity";
 }
