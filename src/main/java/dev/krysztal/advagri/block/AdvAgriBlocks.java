88daeb4635d88081d404c8dbbcbe58ee93f4db12
Krysztal <suibing112233@outlook.com>
2022-12-11 23:01:00 +0800

    🔥 Clean code.

--- src/main/java/dev/krysztal/advagri/block/AdvAgriBlocks.java
+++ src/main/java/dev/krysztal/advagri/block/AdvAgriBlocks.java
@@ -11,7 +11,6 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.MapColor;
 import net.minecraft.block.PillarBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.util.Identifier;
--- src/main/java/dev/krysztal/advagri/foundation/annotation/GenType.java
+++ src/main/java/dev/krysztal/advagri/foundation/annotation/GenType.java
@@ -3,14 +3,18 @@ package dev.krysztal.advagri.foundation.annotation;
 public enum GenType {
   SimpleBlockModel,
   SimpleItemModel,
+  
   SimpleBlockState,
   CropBlockState,
+  
   SimpleBlockLoot,
+  
   MineLevel0,
   MineLevel1,
   MineLevel2,
   MineLevel3,
   MineLevel4,
+
   MineShovel,
   MineAxe,
   MinePickaxe,
